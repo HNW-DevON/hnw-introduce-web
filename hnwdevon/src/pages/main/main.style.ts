56840f47bdc6ecd4f7ffc0a0cda290f274dd9bd5
je355 <chaeeehui@gmail.com>
2024-02-09 22:34:05 +0900

    ⚡ :: main..

---
+++ hnwdevon/src/pages/main/main.style.ts
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const Maintitle = styled.div`
+    color: black;
+    font-weight: 900;
+    font-size: 23px;
+
+    position: absolute;
+    top: 0;
+    margin-top: 80%;
+`
+
+export const TitleWrap = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    
+    width: 100vw;
+`
+
+export const Maintitle2 = styled.div`
+    color: black;
+    font-weight: 900;
+    font-size: 28px;
+
+    position: absolute;
+    top: 0;
+    margin-top: 90%;
+`
+
+export const MainBackground = styled.div`
+    height: 100vh;
+    background-image: white;
+`
+
+export const downloadButton = styled.button`
+    background-color: #097CFA;
+    border-radius: 23px;
+    font-size: 17px;
+    font-weight: 800;
+    color: white;
+
+    width: 170px;
+    height: 40px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+`
+
+export const downloadButtonWrap = styled.div`   
+    position: absolute;
+    top: 0;
+    margin-top: 130%;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    width: 100vw;
+`
--- hnwdevon/src/pages/main/main.tsx
+++ hnwdevon/src/pages/main/main.tsx
@@ -2,10 +2,25 @@ import React, { useEffect, useRef, useState  } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 import Header from "../../components/Header/Header"
+import * as S from "../main/main.style";
 
 const main = () => {
   return (
-    <Header/>
+    <>
+        <Header/>
+        <S.MainBackground>
+        <S.TitleWrap>
+            <S.Maintitle>당신의 올바른 소비를 위해</S.Maintitle>
+        </S.TitleWrap>
+        <S.TitleWrap>
+            <S.Maintitle2>착한소비</S.Maintitle2>
+        </S.TitleWrap>
+        <S.downloadButtonWrap>
+            <S.downloadButton>앱 다운로드</S.downloadButton>
+        </S.downloadButtonWrap>
+        </S.MainBackground>
+        
+    </>
   )
 }
 
