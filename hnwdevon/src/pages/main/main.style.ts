da9797696b4e52bf0ff176579b85af3702d93bf8
je355 <chaeeehui@gmail.com>
2024-02-12 01:06:29 +0900

    ⚡ :: main

--- hnwdevon/src/pages/main/main.style.ts
+++ hnwdevon/src/pages/main/main.style.ts
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Maintitle = styled.div`
     color: black;
     font-weight: 900;
-    font-size: 1.4rem;
+    font-size: 1.7rem;
 
     position: absolute;
     top: 0;
@@ -21,7 +21,7 @@ export const TitleWrap = styled.div`
 export const Maintitle2 = styled.div`
     color: black;
     font-weight: 900;
-    font-size: 2rem;
+    font-size: 2.4rem;
 
     position: absolute;
     top: 0;
@@ -58,6 +58,89 @@ export const downloadButtonWrap = styled.div`
 `
 
 export const MainBackgroundimg = styled.img`
+    margin: 0px;
+    object-fit: cover;
+    width: 100%;
+    display: block;
+`
+
+export const SeBack = styled.img`
+    margin: 0px;
+    object-fit: cover;
+    width: 100%;
+    display: block;
+`
+
+export const AboutHistroyTitle = styled.div`
+    color: black;
+    font-weight: 900;
+    font-size: 2rem;
+
+    position: absolute;
+    top: 0;
+    margin-top: 250%;
+`
+
+export const AboutHistroyTitleWrap = styled.div`
+    position: absolute;
+    top: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    width: 100vw;
+`
+
+export const message1 = styled.img`
+    width: 70%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 290%;
+`
+
+export const message2 = styled.img`
+    width: 58%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 330%;
+    margin-left: 43%;
+`
+
+export const message3 = styled.img`
+    width: 87%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 370%;
+`
+
+export const HistoryStart = styled.div`
+    color: black;
+    font-weight: 900;
+    font-size: 1.3rem;
+
+    position: absolute;
+    top: 0;
+    margin-top: 410%;
+`
+
+export const HistoryStartWrap = styled.div`
+    position: absolute;
+    top: 0;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+
+    width: 100vw;
+`
+
+export const SecondBackImg = styled.img`
     margin: 0px;
     object-fit: cover;
     width: 100%;
--- hnwdevon/src/pages/main/main.tsx
+++ hnwdevon/src/pages/main/main.tsx
@@ -5,6 +5,11 @@ import Header from "../../components/Header/Header"
 import * as S from "../main/main.style";
 
 import FistBackground from "../../assets/image/background1.svg"
+import HisMessage1 from "../../assets/image/histroymessage1.svg"
+import HisMessage2 from "../../assets/image/histroymessage2.svg"
+import HisMessage3 from "../../assets/image/histroymessage3.svg"
+import WhiteBack from "../../assets/image/whiteback.svg"
+import SecondBackground from "../../assets/image/3back.svg"
 
 const main = () => {
   return (
@@ -22,6 +27,19 @@ const main = () => {
             <S.downloadButton>앱 다운로드</S.downloadButton>
         </S.downloadButtonWrap>
         </>
+        <S.SeBack src={WhiteBack}/>
+        <> 
+          <S.AboutHistroyTitleWrap>
+            <S.AboutHistroyTitle>착한소비는</S.AboutHistroyTitle>
+          </S.AboutHistroyTitleWrap>
+          <S.message1 src={HisMessage1}></S.message1>
+          <S.message2 src={HisMessage2}></S.message2>
+          <S.message3 src={HisMessage3}></S.message3>
+          <S.HistoryStartWrap>
+            <S.HistoryStart>라는 의문점을 가지고 시작되었어요</S.HistoryStart>
+          </S.HistoryStartWrap>
+        </>
+        <S.SecondBackImg src={SecondBackground}/>
     </>
   )
 }
