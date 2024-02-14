f064303196cc8b0f0fe143d8986901321d10ba8b
je355 <chaeeehui@gmail.com>
2024-02-14 15:19:36 +0900

    ⚡ :: stack

--- hnwdevon/src/pages/member/member.style.ts
+++ hnwdevon/src/pages/member/member.style.ts
@@ -114,6 +114,8 @@ export const HnwMessage2st = styled.img`
     top: 0;
 `
 
+export const AllLogo = styled.div`
+`
 
 export const HnwMessage4 = styled.div`
     width: 60%;
@@ -147,4 +149,241 @@ export const HistroyStartWrap = styled.div`
     width: 100vw;
     display: flex;
     justify-content: center;
+`
+
+export const Consume = styled.div`
+    color: black;
+    font-weight: 900;
+    font-size: 1.7rem;
+    position: absolute;
+    top: 0;
+    margin-top: 480%;
+
+    line-height: 0;
+`
+
+export const ConsumeEx = styled.p`
+    color: #097CFA;
+    font-weight: 900;
+    font-size: 1.7rem;
+    position: absolute;
+    top: 0;
+`
+
+export const ConsumeWrap = styled.div`
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+`
+
+export const ExplainCosume = styled.div`
+    color: black;
+    font-weight: 600;
+    font-size: 1rem;
+    position: absolute;
+    top: 0;
+    margin-top: 500%;
+`
+
+export const ExplainCosumeWrap = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const ExplainCosume1 = styled.div`
+    color: black;
+    font-weight: 600;
+    font-size: 1rem;
+    position: absolute;
+    top: 0;
+    margin-top: 550%;
+`
+
+export const ExplainCosume2 = styled.div`
+    color: black;
+    font-weight: 800;
+    font-size: 1.2rem;
+    position: absolute;
+    top: 0;
+    margin-top: 620%;
+`
+
+export const swiftUi = styled.div`
+    width: 20%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 700%;
+    margin-left: 25%;
+`
+
+export const swiftUi1 = styled.img`
+    width: 100%;
+`
+
+export const ReactLogo = styled.div`
+    width: 28%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 700%;
+    margin-left: 50%;
+`
+
+export const ReactLogo1 = styled.img`
+  width: 100%;
+`
+
+export const style = styled.div`
+    width: 20%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 730%;
+    margin-left: 13%;
+`
+
+export const style1 = styled.img`
+  width: 100%;
+`
+
+export const Alamofire = styled.div`
+    width: 17%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 723%;
+    margin-left: 36%;
+`
+
+export const Alamofire1 = styled.img`
+    width: 100%;
+`
+
+export const springboot = styled.div`
+  width: 24%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 730%;
+    margin-left: 57%;
+`
+
+export const springboot1 = styled.img`
+  width: 100%;
+`
+
+export const mysql = styled.div`
+    width: 30%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 747%;
+    margin-left: 17%;
+`
+
+export const mysql1 = styled.img`
+    width: 100%;
+`
+
+export const ts = styled.div`
+    width: 20%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 770%;
+    margin-left: 17%;
+`
+
+export const ts1 = styled.img`
+    width: 100%;
+`
+
+export const router = styled.div`
+    width: 20%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 765%;
+    margin-left: 45%;
+`
+
+export const router1 = styled.img`
+    width: 100%;
+`
+
+export const Tuist = styled.div`
+    width: 17%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 755%;
+    margin-left: 68%;
+`
+
+export const Tuist1 = styled.img`
+    width: 100%;
+`
+
+export const github = styled.div`
+    width: 17%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 795%;
+    margin-left: 20%;
+`
+
+export const github1 = styled.img`
+    width: 100%;
+`
+
+export const Figma = styled.div`
+    width: 12%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 785%;
+    margin-left: 45%;
+`
+
+export const Figma1 = styled.img`
+    width: 100%;
+`
+
+export const Notion = styled.div`
+    width: 21%;
+
+    position: absolute;
+    top: 0;
+    margin-top: 787%;
+    margin-left: 67%;
+`
+
+export const Notion1 = styled.img`
+    width: 100%;
+`
+
+export const useStack = styled.div`
+    color: black;
+    font-weight: 800;
+    font-size: 2.1rem;
+    position: absolute;
+    top: 0;
+    margin-top: 840%;
+`
+
+export const useStackWrap = styled.div`
+    display: flex;
+    justify-content: center;
+`
+
+export const Member = styled.div`
+    color: #097CFA;
+    font-weight: 900;
+    font-size: 2.6rem;
+    position: absolute;
+    top: 0;
+    margin-top: 900%;
+    margin-left: 13%;
 `
--- hnwdevon/src/pages/member/member.tsx
+++ hnwdevon/src/pages/member/member.tsx
@@ -10,6 +10,19 @@ import HnwMessage1 from "../../assets/image/howabouthnw.svg"
 import HnwMessage2 from "../../assets/image/appsocai.svg"
 import HnwMessage3 from "../../assets/image/needmessage.svg"
 import HnwMessage4 from "../../assets/image/socall.svg"
+import swiftUi from "../../assets/image/swiftui.svg"
+import ReactLogo from "../../assets/image/react.svg"
+import style from "../../assets/image/style.svg"
+import Alamofire from "../../assets/image/swfit.svg"
+import springboot from "../../assets/image/springboot.svg"
+import mysql from "../../assets/image/mysql.svg"
+import ts from "../../assets/image/ts.svg"
+import router from "../../assets/image/router.svg"
+import Tuist from "../../assets/image/Tuist.svg"
+import github from "../../assets/image/github.svg"
+import Figma from "../../assets/image/figma.svg"
+import Notion from "../../assets/image/notion.svg"
+
 
 const member: React.FC = () => {
   const navigate = useNavigate();
@@ -20,6 +33,23 @@ const member: React.FC = () => {
   const animatedElementRef2 = useRef<HTMLDivElement>(null);
   const animatedElementRef3 = useRef<HTMLDivElement>(null);
   const animatedElementRef4 = useRef<HTMLDivElement>(null);
+  const animatedElementRef5 = useRef<HTMLDivElement>(null);
+  const animatedElementRef6 = useRef<HTMLDivElement>(null);
+  const animatedElementRef7 = useRef<HTMLDivElement>(null);
+  const animatedElementRef8 = useRef<HTMLDivElement>(null);
+  const animatedElementRef9 = useRef<HTMLDivElement>(null);  
+  const animatedElementRef10 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef11 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef12 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef13 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef14 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef15 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef16 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef17 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef18 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef19 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef20 = useRef<HTMLDivElement>(null); 
+  const animatedElementRef21 = useRef<HTMLDivElement>(null); 
 
   const [visibleElements, setVisibleElements] = useState<number>(0);
 
@@ -29,18 +59,68 @@ const member: React.FC = () => {
       const { top: top2 } = animatedElementRef2.current!.getBoundingClientRect();
       const { top: top3 } = animatedElementRef3.current!.getBoundingClientRect();
       const { top: top4 } = animatedElementRef4.current!.getBoundingClientRect();
-
+      const { top: top5 } = animatedElementRef5.current!.getBoundingClientRect();
+      const { top: top6 } = animatedElementRef6.current!.getBoundingClientRect();
+      const { top: top7 } = animatedElementRef7.current!.getBoundingClientRect();
+      const { top: top8 } = animatedElementRef8.current!.getBoundingClientRect();
+      const { top: top9 } = animatedElementRef9.current!.getBoundingClientRect(); 
+      const { top: top10 } = animatedElementRef10.current!.getBoundingClientRect(); 
+      const { top: top11 } = animatedElementRef11.current!.getBoundingClientRect(); 
+      const { top: top12 } = animatedElementRef12.current!.getBoundingClientRect();
+      const { top: top13 } = animatedElementRef13.current!.getBoundingClientRect();
+      const { top: top14 } = animatedElementRef14.current!.getBoundingClientRect();
+      const { top: top15 } = animatedElementRef15.current!.getBoundingClientRect();
+      const { top: top16 } = animatedElementRef16.current!.getBoundingClientRect();
+      const { top: top17 } = animatedElementRef17.current!.getBoundingClientRect();
+      const { top: top18 } = animatedElementRef18.current!.getBoundingClientRect();
+      const { top: top19 } = animatedElementRef19.current!.getBoundingClientRect();
+      const { top: top20 } = animatedElementRef20.current!.getBoundingClientRect();
+      const { top: top21 } = animatedElementRef21.current!.getBoundingClientRect();
      
       const isInViewPort1 = top1 >= 0 && top1 <= window.innerHeight;
       const isInViewPort2 = top2 >= 0 && top2 <= window.innerHeight;
       const isInViewPort3 = top3 >= 0 && top3 <= window.innerHeight;
       const isInViewPort4 = top4 >= 0 && top4 <= window.innerHeight;
+      const isInViewPort5 = top5 >= 0 && top5 <= window.innerHeight;
+      const isInViewPort6 = top6 >= 0 && top6 <= window.innerHeight;
+      const isInViewPort7 = top7 >= 0 && top7 <= window.innerHeight;
+      const isInViewPort8 = top8 >= 0 && top8 <= window.innerHeight;
+      const isInViewPort9 = top9 >= 0 && top9 <= window.innerHeight;
+      const isInViewPort10 = top10 >= 0 && top10 <= window.innerHeight;
+      const isInViewPort11 = top11 >= 0 && top11 <= window.innerHeight;
+      const isInViewPort12 = top12 >= 0 && top12 <= window.innerHeight;
+      const isInViewPort13 = top13 >= 0 && top13 <= window.innerHeight;
+      const isInViewPort14 = top14 >= 0 && top14 <= window.innerHeight;
+      const isInViewPort15 = top15 >= 0 && top15 <= window.innerHeight;
+      const isInViewPort16 = top16 >= 0 && top16 <= window.innerHeight;
+      const isInViewPort17 = top17 >= 0 && top17 <= window.innerHeight;
+      const isInViewPort18 = top18 >= 0 && top18 <= window.innerHeight;
+      const isInViewPort19 = top19 >= 0 && top19 <= window.innerHeight;
+      const isInViewPort20 = top20 >= 0 && top20 <= window.innerHeight;
+      const isInViewPort21 = top21 >= 0 && top21 <= window.innerHeight;
      
       let visibleCount = 0;
       if (isInViewPort1) visibleCount = 1;
       if (isInViewPort2) visibleCount = 2;
       if (isInViewPort3) visibleCount = 3;
       if (isInViewPort4) visibleCount = 4;
+      if (isInViewPort5) visibleCount = 5;
+      if (isInViewPort6) visibleCount = 6;
+      if (isInViewPort7) visibleCount = 7;
+      if (isInViewPort8) visibleCount = 8;
+      if (isInViewPort9) visibleCount = 9;
+      if (isInViewPort10) visibleCount = 10;
+      if (isInViewPort11) visibleCount = 11;
+      if (isInViewPort12) visibleCount = 12;
+      if (isInViewPort13) visibleCount = 13;
+      if (isInViewPort14) visibleCount = 14;
+      if (isInViewPort15) visibleCount = 15;
+      if (isInViewPort16) visibleCount = 16;
+      if (isInViewPort17) visibleCount = 17;
+      if (isInViewPort18) visibleCount = 18;
+      if (isInViewPort19) visibleCount = 19;
+      if (isInViewPort20) visibleCount = 20
+      if (isInViewPort21) visibleCount = 21;
   
       setVisibleElements(visibleCount);
     };
@@ -104,6 +184,175 @@ const member: React.FC = () => {
         <S.HistroyStart>라는 의견들이 모여 시작되었어요</S.HistroyStart>
       </S.HistroyStartWrap>
       <S.WhiteBackground src={WhiteBack}/>
+      <S.ConsumeWrap>
+        <S.Consume
+        ref={animatedElementRef5}
+        style={{
+          opacity: visibleElements >= 5 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 5 ? "0" : "50px"})`,
+         transition: "opacity 0.5s, transform 3s", 
+        }}
+        > Until everyone can <S.ConsumeEx>consume well</S.ConsumeEx> </S.Consume>
+      </S.ConsumeWrap>
+      <S.ExplainCosumeWrap>
+        <S.ExplainCosume
+        ref={animatedElementRef6}
+        style={{
+          opacity: visibleElements >= 6 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 6 ? "0" : "50px"})`,
+         transition: "opacity 0.5s, transform 2.4s", 
+        }}
+        >데브온은 '모두가 착한 소비를 할 수 있을때까지' <br/> 라는 비전을 가진 팀입니다. 
+          <br/>많은 사람들이 사회적기업이 무엇인지 알지만 <br/> 소비로 이어지지 않는다는 점을
+          <br/>저희는 발견했습니다.
+        </S.ExplainCosume>
+      </S.ExplainCosumeWrap>
+
+      <S.ExplainCosumeWrap>
+        <S.ExplainCosume1
+        ref={animatedElementRef7}
+        style={{
+          opacity: visibleElements >= 7 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 7 ? "0" : "50px"})`,
+         transition: "opacity 0.5s, transform 2.4s", 
+        }}>착한 소비의 핵심은 <br/> 기업과 유저의 양방향 공급이라는 점에 있습니다.<br/>
+        사회적기업과 사람을 이어주는 콘텐츠, <br/> 소비자가 즐겁게 착한소비를 할 수 있도록.
+        </S.ExplainCosume1>
+      </S.ExplainCosumeWrap>
+
+      <S.ExplainCosumeWrap>
+        <S.ExplainCosume2
+        ref={animatedElementRef8}
+        style={{
+          opacity: visibleElements >= 8 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 8 ? "0" : "50px"})`,
+         transition: "opacity 0.5s, transform 3.2s", 
+        }}>모두가 착한 소비를 할 수 있을때까지
+        </S.ExplainCosume2>
+      </S.ExplainCosumeWrap>
+      <S.WhiteBackground src={WhiteBack}/>
+      <S.AllLogo>
+        <S.swiftUi
+        ref={animatedElementRef9}
+        style={{
+          opacity: visibleElements >= 9 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 9 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 2.3s", 
+        }}>
+          <S.swiftUi1 src={swiftUi}/>
+        </S.swiftUi>
+        <S.ReactLogo
+        ref={animatedElementRef10}
+        style={{
+          opacity: visibleElements >= 10 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 10 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 3s", 
+        }}>
+          <S.ReactLogo1 src={ReactLogo}/>
+        </S.ReactLogo>
+        <S.style
+        ref={animatedElementRef11}
+        style={{
+          opacity: visibleElements >= 11 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 11 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 2.3s", 
+        }}>
+          <S.style1 src={style}/>
+        </S.style>
+        <S.Alamofire
+        ref={animatedElementRef12}
+        style={{
+          opacity: visibleElements >= 12 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 12 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 1.5s", 
+        }}>
+          <S.Alamofire1 src={Alamofire}/>
+        </S.Alamofire>
+        <S.springboot
+        ref={animatedElementRef13}
+        style={{
+          opacity: visibleElements >= 13? 1 : 0,
+          transform: `translateY(${visibleElements >= 13 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 2.7s", 
+        }}>
+          <S.springboot1 src={springboot}/>
+        </S.springboot>
+        <S.mysql
+        ref={animatedElementRef14}
+        style={{
+          opacity: visibleElements >= 14 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 14 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 3.1s", 
+        }}>
+          <S.mysql1 src={mysql}/>
+        </S.mysql>
+        <S.ts
+        ref={animatedElementRef15}
+        style={{
+          opacity: visibleElements >= 15 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 15 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 2.3s", 
+        }}>
+          <S.ts1 src={ts}/>
+        </S.ts>
+        <S.router
+        ref={animatedElementRef16}
+        style={{
+          opacity: visibleElements >= 16 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 16 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 1.9s", 
+        }}>
+          <S.router1 src={router}/>
+        </S.router>
+        <S.Tuist
+        ref={animatedElementRef17}
+        style={{
+          opacity: visibleElements >= 17 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 17 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 2.7s", 
+        }}>
+          <S.Tuist1 src={Tuist}/>
+        </S.Tuist>
+        <S.github
+        ref={animatedElementRef18}
+        style={{
+          opacity: visibleElements >= 18 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 18 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 1.7s", 
+        }}>
+          <S.github1 src={github}/>
+        </S.github>
+        <S.Figma
+        ref={animatedElementRef19}
+        style={{
+          opacity: visibleElements >= 19 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 19 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 3s", 
+        }}>
+          <S.Figma1 src={Figma}/>
+        </S.Figma>
+        <S.Notion
+        ref={animatedElementRef20}
+        style={{
+          opacity: visibleElements >= 20 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 20 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 2.3s", 
+        }}>
+          <S.Notion1 src={Notion}/>
+        </S.Notion>
+      </S.AllLogo>
+      <S.useStackWrap>
+        <S.useStack
+        ref={animatedElementRef21}
+        style={{
+          opacity: visibleElements >= 21 ? 1 : 0,
+          transform: `translateY(${visibleElements >= 21 ? "0" : "50px"})`,
+        transition: "opacity 0.5s, transform 2.5s", 
+        }}
+        >STACK</S.useStack>
+      </S.useStackWrap>
+      <S.WhiteBackground src={WhiteBack}/>
+      <S.Member>Member</S.Member>
     </>
   )
 }
